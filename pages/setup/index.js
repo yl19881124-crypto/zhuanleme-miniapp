84f8917a328f2304f503d49fd530971aef92d6ee
yl19881124-crypto <yl19881124@gmail.com>
2026-05-09 22:52:32 +0800

    fix: unify config/privacy storage and clear today day_state

--- pages/setup/index.js
+++ pages/setup/index.js
@@ -1,4 +1,5 @@
 const { KEYS, set } = require('../../utils/storage');
+const { saveConfig, DEFAULT_CONFIG } = require('../../utils/day-store');
 
 Page({
   data: {
@@ -11,12 +12,22 @@ Page({
 
   saveConfig() {
     const monthlySalary = Number(this.data.monthlySalary || 0);
-    const hourSalary = monthlySalary / this.data.workDaysPerMonth / this.data.workHoursPerDay;
-    set(KEYS.PROFILE, {
+    const profile = {
       monthlySalary,
-      hourSalary,
+      hourSalary: monthlySalary / this.data.workDaysPerMonth / this.data.workHoursPerDay,
       daySalary: monthlySalary / this.data.workDaysPerMonth,
       workHoursPerDay: this.data.workHoursPerDay
+    };
+    set(KEYS.PROFILE, profile);
+    saveConfig({
+      monthlySalary,
+      workdaysPerMonth: this.data.workDaysPerMonth,
+      startTime: DEFAULT_CONFIG.startTime,
+      endTime: DEFAULT_CONFIG.endTime,
+      lunchStart: DEFAULT_CONFIG.lunchStart,
+      lunchEnd: DEFAULT_CONFIG.lunchEnd,
+      lunchPaid: DEFAULT_CONFIG.lunchPaid,
+      weekendPaid: DEFAULT_CONFIG.weekendPaid
     });
     set(KEYS.INITIALIZED, true);
     wx.switchTab ? wx.switchTab({ url: '/pages/home/index' }) : wx.redirectTo({ url: '/pages/home/index' });
